e5bf7e4b390e31d327de15f9c7ac6238a19163b3
parmeetji <parmeetchabra3@gmail.com>
2026-06-14 14:23:17 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="engine_health_prediction_nb/deployment",     # the local folder containing your files
+    folder_path="deployment",     # the local folder containing your files
     repo_id="Pammi123/engine-health-prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
